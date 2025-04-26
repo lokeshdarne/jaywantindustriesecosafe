99abe2640c168ca2966aca57bb2e91a5b2dacf2c
lokeshdarne <lokeshdarne@gmail.com>
2025-04-26 15:17:38 +0530

    new look

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  site: 'https://www.jaywantindustriesecosafe.in', // Your custom domain
-  integrations: [tailwind()]
-});
